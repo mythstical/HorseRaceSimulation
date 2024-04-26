2503db73e19e841874b97c800f70f38858b22954
roze <rosegems03@gmail.com>
2024-04-26 22:40:42 +0100

    Create HorseColor enum to define possible coat colors for horses

---
+++ core/HorseColor.java
@@ -0,0 +1,14 @@
+public enum HorseColor {
+
+    CHESTNUT("#954535"),
+    GREY("#808080"),
+    BAY("#67412B"),
+    BLACK("#000000"),
+    PALOMINO("#C79F70");
+
+    public final String value;
+
+    private HorseColor(String value) {
+        this.value = value;
+    }
+}
