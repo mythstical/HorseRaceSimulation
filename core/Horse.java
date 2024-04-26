57d2996b4edd0be636874abea53654ed5e8b0505
roze <rosegems03@gmail.com>
2024-04-26 22:06:02 +0100

    add serialisable

--- core/Horse.java
+++ core/Horse.java
@@ -1,27 +1,53 @@
+package core;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.Serializable;
+
 
 /**
- * Write a description of class Horse here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * A three-horse race, each horse running in its own lane
+ * for a given distance
+ *
+ * @author Roze Behzad
+ * @version 1.0
  */
-public class Horse
+
+public class Horse implements Serializable
 {
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
+    private boolean fallen;
+    private double confidence;
+    private char symbol;
+    private String name;
+    private int distanceTravelled;
+    private Breed breed;
+    private HorseColor coatColor;
+    private int racesWon;
+    private int racesLost;
+    private int totalDistanceTravelled;
+    private int totalRunningTime;
+    private int imgFrameChanger = 0;
+
+
+    //Constructor of class Core.Horse
     /**
-     * Constructor for objects of class Horse
+     * Constructor for objects of class Core.Horse
      */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    public Horse(char horseSymbol, String horseName,  double horseConfidence, Breed breed, HorseColor color)
     {
-       
+        this.fallen = false;
+        this.confidence = horseConfidence;
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.breed = breed;
+        this.coatColor = color;
+        this.distanceTravelled = 0;
     }
-    
-    
-    
+
+
+
+
     //Other methods of class Horse
     public void fall()
     {
